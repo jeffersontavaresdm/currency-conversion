39c4050219a503e9120c33f8f0e1378f4283f8ba
jeffersontdm <jeffersontavares1611@gmail.com>
2022-12-05 22:54:01 -0300

    change base url to application file

--- src/main/java/com/currencyconversion/api/CurrencyConverterAPIConfig.java
+++ src/main/java/com/currencyconversion/api/CurrencyConverterAPIConfig.java
@@ -2,6 +2,7 @@ package com.currencyconversion.api;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -9,6 +10,9 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
 public class CurrencyConverterAPIConfig {
+
+  @Value("${api.baseUrl}")
+  private String baseUrl;
 
   @Bean
   public CurrencyConverterAPI awesomeApi() {
@@ -18,7 +22,7 @@ public class CurrencyConverterAPIConfig {
 
     return new Retrofit
       .Builder()
-      .baseUrl("https://economia.awesomeapi.com.br")
+      .baseUrl(baseUrl)
       .addConverterFactory(JacksonConverterFactory.create(mapper))
       .build()
       .create(CurrencyConverterAPI.class);
