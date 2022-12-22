379c8a9c982c88c1723e5bc4fbe4bd09c3edd33e
jeffersontdm <jeffersontavares1611@gmail.com>
2022-12-22 09:39:45 -0300

    add cors config

---
+++ src/main/java/com/currencyconversion/configs/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.currencyconversion.configs;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins("*");
+      }
+    };
+  }
+}
